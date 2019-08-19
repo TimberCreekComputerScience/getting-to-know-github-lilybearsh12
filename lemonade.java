6db72dcd75cbb1784e234c4df6603773cdd60fa3
lilybearsh12 <54044517+lilybearsh12@users.noreply.github.com>
2019-08-19 12:17:42 -0400

    Add files via upload

---
+++ lemonade.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+     public static void main(String []args){
+        Scanner scan = new Scanner(System.in); 
+        System.out.println("Sup, how many cups of lemonade do you want?");
+        int count = scan.nextInt();
+        
+        double cost = 1.00;
+        double total = count * cost;
+        System.out.printf("Your total will be $%.2f\n",total);
+     }    
+}
